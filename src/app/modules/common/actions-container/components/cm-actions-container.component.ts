8af513fe4c16f8abb02374ee0950f56e494e8d13
dmda0418 <dmytro.davydenko@netcracker.com>
2020-02-05 01:24:14 +0200

    playing with container animation

--- src/app/modules/common/actions-container/components/cm-actions-container.component.ts
+++ src/app/modules/common/actions-container/components/cm-actions-container.component.ts
@@ -1,9 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'cm-actions-container',
   templateUrl: './cm-actions-container.component.html',
-  styleUrls: ['./cm-actions-container.component.less']
+  styleUrls: ['./cm-actions-container.component.less'],
+  animations: [
+    trigger('showHide', [
+      state('show', style({
+        top: '0px',
+        left: '0px',
+        height: '100%',
+        width: '100%',
+        'z-index': 100,
+        'background-color': 'red',
+      })),
+      state('hide', style({
+        width: '100%',
+        bottom: '0px',
+        left: '0px',
+        })),
+      transition('hide <=> show', [
+        animate('1s')
+      ]),
+    ]),
+  ]
 })
 export class CmActionsContainerComponent implements OnInit {
 
